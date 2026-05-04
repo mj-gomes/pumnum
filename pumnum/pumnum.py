b6b5646cc8a144383578aad36e5258972b59f416
Miguel José Gomes <migueltgomes@proton.me>
2026-05-04 17:10:52 +0200

    add njit args to pumnum decorator, update notebook, add test for parallel njit case

--- pumnum/pumnum.py
+++ pumnum/pumnum.py
@@ -1,8 +1,9 @@
-import unyt
 import functools
 from contextlib import contextmanager
-from numba import njit
+
 import pint
+import unyt
+from numba import njit
 
 """
 The following functions limit loops to a single iteration.
@@ -12,6 +13,7 @@ pre-computed
 """
 
 _loop_once = False
+
 
 @contextmanager
 def limit_loops():
@@ -24,12 +26,15 @@ def limit_loops():
     finally:
         _loop_once = old_value
 
+
 def run_loop_once(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         with limit_loops():
             return func(*args, **kwargs)
+
     return wrapper
+
 
 """"""
 
@@ -39,22 +44,31 @@ final units of the numba jitted function, and then compute it
 and give it the pre-computed units
 """
 
-def pumnum(func):
-    def __init__(*args, **kwargs):
-        args_magnitudes = []
-        args_units = []
-        for arg in args:
-            if isinstance(arg, pint.Quantity):
-                args_magnitudes.append(arg.magnitude)
-            elif isinstance(arg, unyt.array.unyt_quantity):
-                args_magnitudes.append(arg)
-            else:
-                return
-            args_units.append(1*arg.units)
 
-        tmp = run_loop_once(func)
-        result = tmp(*args_units)
-        final_units = result.units
-        op=njit(func)
-        return op(*args_magnitudes) * final_units
-    return __init__
+def pumnum(_func=None, **njit_kwargs):
+    def decorator(func):
+        @functools.wraps(func)
+        def __init__(*args, **kwargs):
+            args_magnitudes = []
+            args_units = []
+            for arg in args:
+                if isinstance(arg, pint.Quantity):
+                    args_magnitudes.append(arg.magnitude)
+                elif isinstance(arg, unyt.array.unyt_quantity):
+                    args_magnitudes.append(arg)
+                else:
+                    return
+                args_units.append(1 * arg.units)
+
+            tmp = run_loop_once(func)
+            result = tmp(*args_units)
+            final_units = result.units
+            op = njit(**njit_kwargs)(func)
+            return op(*args_magnitudes) * final_units
+
+        return __init__
+
+    if _func is None:
+        return decorator
+    else:
+        return decorator(_func)
